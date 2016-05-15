44f4ba850cf6571276053ef732b5afe7ba809ddf
paramaxproduction <paramaxproduction@gmail.com>
2016-05-15 17:48:49 -0400

    Contact list

---
+++ ReactTest/src/App.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ContactsList from './ContactsList';
+
+let contacts = {
+  name: 'Maxime',
+  phone: '555 553 5325'
+}
+
+class App extends React.Component {
+  render() {
+    return (
+      <div>
+        <h1>Contacts List</h1>
+        <ContactsList contacts={this.props.contacts}/>
+      </div>
+    )
+  }
+}
+
+React.render(<App contacts={contacts}/>, document.getElementById('app'));
