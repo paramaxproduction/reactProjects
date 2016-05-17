3c0f4063621763e626689ef068a7c0f9042f36ed
paramaxproduction <paramaxproduction@gmail.com>
2016-05-16 20:02:13 -0400

    continued

--- ReactTest/src/App.js
+++ ReactTest/src/App.js
@@ -1,10 +1,23 @@
 import React from 'react';
 import ContactsList from './ContactsList';
 
-let contacts = {
+let contacts = [{
+  id: 1,
   name: 'Maxime',
   phone: '555 553 5325'
-}
+}, {
+  id: 2,
+  name: 'Henri',
+  phone: '435 593 0023'
+}, {
+  id: 3,
+  name: 'Kristelle',
+  phone: '902 245 872'
+}, {
+  id: 4,
+  name: 'Lynch',
+  phone: '450 726 4443'
+}]
 
 class App extends React.Component {
   render() {
