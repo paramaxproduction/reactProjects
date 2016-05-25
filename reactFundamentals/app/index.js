b5412c5d1a8b4210cb27e854c756ce4cea6bbb8a
paramaxproduction <paramaxproduction@gmail.com>
2016-05-25 15:51:56 -0400

    react fun

--- reactFundamentals/app/index.js
+++ reactFundamentals/app/index.js
@@ -1,15 +1,49 @@
+var USER_DATA = {
+  name: 'Maxime St-jean',
+  username: 'paramaxproduction',
+  image: 'https://avatars.githubusercontent.com/paramaxproduction'
+}
+
 var React = require('react');
 var ReactDom = require('react-dom');
 
-var HelloWorld = React.createClass({
+var ProfilePic = React.createClass({
   render: function() {
-    return(
-      <div> Hello World </div>
+    return <img src={this.props.imageUrl} style={{height: 100, width: 100}} />
+  }
+});
+
+var ProfileLink = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <a href={'https://www.github.com/' + this.props.username}>{this.props.username}</a>
+      </div>
     )
   }
 });
 
+var ProfileName = React.createClass({
+  render: function() {
+    return (
+      <div>{this.props.name}</div>
+    )
+  }
+});
+
+var Avatar = React.createClass({
+  render: function() {
+    return (
+      <div>
+        <ProfilePic imageUrl={this.props.user.image} />
+        <ProfileName name={this.props.user.name} />
+        <ProfileLink username={this.props.user.username} />
+      </div>
+    )
+  }
+})
+
 ReactDom.render(
-  <HelloWorld />,
+  <Avatar user={USER_DATA} />,
   document.getElementById('app')
 );
